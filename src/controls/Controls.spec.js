0edf9bd50589df0d54c956c128847fdf7908d462
ykrueng <yasirah.krueng@gmail.com>
2019-01-30 16:12:54 -0600

    add comment to test suit for Control component

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,6 +1,6 @@
 // Test away!
 import React from 'react';
-import { render, fireEvent } from 'react-testing-library';
+import { render } from 'react-testing-library';
 import 'jest-dom/extend-expect';
 
 import Controls from './Controls';
@@ -8,10 +8,12 @@ import Controls from './Controls';
 describe('<Controls />', () => {
   describe('Lock Control', () => {
     it('rerenders lock', () => {
+      // initialize to unlocked
       const { getByText, rerender } = render(<Controls locked={false} />);
       let lock = getByText('Lock Gate');
       expect(lock.textContent).toBe('Lock Gate');
 
+      // rerender to locked
       rerender(<Controls locked={true} />);
       lock = getByText('Unlock Gate');
       expect(lock.textContent).toBe('Unlock Gate');
@@ -20,10 +22,12 @@ describe('<Controls />', () => {
 
   describe('Gate Control', () => {
     it('rerenders gate', () => {
+      // initialize to opened
       const { getByText, rerender } = render(<Controls closed={false} />);
       let gate = getByText('Close Gate');
       expect(gate.textContent).toBe('Close Gate');
 
+      // rerender to closed
       rerender(<Controls closed={true} />);
       gate = getByText('Open Gate');
       expect(gate.textContent).toBe('Open Gate');
