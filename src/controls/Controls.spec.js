87bb259d0a748914240ad92c68d0f5835afa0950
ykrueng <yasirah.krueng@gmail.com>
2019-01-30 16:09:13 -0600

    add test suit for Control component

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,32 @@
 // Test away!
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+
+import Controls from './Controls';
+
+describe('<Controls />', () => {
+  describe('Lock Control', () => {
+    it('rerenders lock', () => {
+      const { getByText, rerender } = render(<Controls locked={false} />);
+      let lock = getByText('Lock Gate');
+      expect(lock.textContent).toBe('Lock Gate');
+
+      rerender(<Controls locked={true} />);
+      lock = getByText('Unlock Gate');
+      expect(lock.textContent).toBe('Unlock Gate');
+    });
+  });
+
+  describe('Gate Control', () => {
+    it('rerenders gate', () => {
+      const { getByText, rerender } = render(<Controls closed={false} />);
+      let gate = getByText('Close Gate');
+      expect(gate.textContent).toBe('Close Gate');
+
+      rerender(<Controls closed={true} />);
+      gate = getByText('Open Gate');
+      expect(gate.textContent).toBe('Open Gate');
+    });
+  });
+})
